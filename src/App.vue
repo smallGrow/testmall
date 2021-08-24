e013bbc3bc2dac184db24b157764dc15158909cd
smallGrow <2515705984@qq.com>
2021-08-24 22:21:38 +0800

    添加一个组件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <jin-li-object></jin-li-object>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JinLiObject from "./components/JinLiObject";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JinLiObject
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "css/jinLicss/jinLIcss.css";
 </style>
---
+++ src/components/JinLiObject.vue
@@ -0,0 +1,56 @@
+<template>
+<!--    导航条开始-->
+<!--    最外层的大盒子-->
+    <div class="nav">
+<!--        水平居中的盒子-->
+        <div class="wrap">
+<!--            左侧盒子-->
+            <div class="logo">
+                <a href="#">
+                <img src="../img/jinLiImage/logo.png" alt="">
+                </a>
+            </div>
+<!--            右侧盒子-->
+            <div class="nav-bar">
+<!--                上面的盒子-->
+                <div class="nav-bar-top">
+                    <ul>
+                        <li><a href="#" class="active">amigo账号</a><span>|</span></li>
+                        <li><a href="#">原账号</a><span>|</span></li>
+                        <li><a href="#">注册</a></li>
+                        <li><a href="#" class="cart">购物车(0)</a></li>
+                    </ul>
+                </div>
+<!--                下面的盒子-->
+                <div class="nav-bar-bottom">
+                    <ul>
+                        <li><a href="#">首页</a></li>
+                        <li><a href="#">手机</a></li>
+                        <li><a href="#">配件</a></li>
+                        <li><a href="#">服务</a></li>
+                        <li><a href="#">下载</a></li>
+                        <li><a href="#">amigoOS</a></li>
+                        <li class="form">
+                            <form action="">
+                                <input type="text" placeholder="输入搜索内容" >
+                                <a href="#"><button>镜</button></a>
+                                <div class="search"></div>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "JinLiObject"
+    }
+</script>
+
+<style scoped>
+
+
+</style>
