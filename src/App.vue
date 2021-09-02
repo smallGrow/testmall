b2a22465adb187d0f30f74a482a333af261350b5
smallGrow <2515705984@qq.com>
2021-09-02 08:12:17 +0800

    添加一个组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <jin-li-object></jin-li-object>
+  <bai-du></bai-du>
 <!--  <test></test>-->
 </template>
 
 <script>
-import JinLiObject from "./components/JinLiObject";
+// import JinLiObject from "./components/JinLiObject";
 // import test from "./components/test";
+import BaiDu from "./components/BaiDu";
 
 export default {
   name: 'App',
   components: {
     // test
-    JinLiObject
+    BaiDu
   }
 }
 </script>
 
 <style>
   @import "css/jinLicss/jinLIcss.css";
+  @import "css/biadu/bdstyle.css";
 </style>
---
+++ src/components/BaiDu.vue
@@ -0,0 +1,133 @@
+<template>
+<!--    三栏布局：-->
+<!--    左右两侧宽度固定-->
+<!--    中间的宽度跟着浏览器的缩放而缩放-->
+<!--    导航条开始-->
+    <div class="nav">
+        <div class="arrow">
+            <img src="../img/baiduImage/百度周边1.png" alt="">
+        </div>
+        <div class="titler">
+            <img src="../img/baiduImage/百度周边5.png" alt="">
+            <span>周边服务</span>
+        </div>
+        <div class="searcher"><img src="../img/baiduImage/百度周边2.png" alt=""></div>
+    </div>
+<!--    导航条结束-->
+<!--    美食开始-->
+    <div class="food">
+        <div>
+            <a href="#">
+                <img src="../img/baiduImage/food1.png" alt="">
+                <p>美食</p>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="../img/baiduImage/food2.png" alt="">
+                <p>风景</p>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="../img/baiduImage/food3.png" alt="">
+                <p>酒店</p>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="../img/baiduImage/food4.png" alt="">
+                <p>休闲娱乐</p>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="../img/baiduImage/food15.png" alt="">
+                <p>车主服务</p>
+            </a>
+        </div>
+    </div>
+<!--    美食结束-->
+<!--    菜单开始-->
+    <div class="menu">
+        <div class="menu-top">
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+        </div>
+        <div class="menu-bottom">
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img src="../img/baiduImage/menu1.png" alt="">
+                    <p>美食</p>
+                </a>
+            </div>
+        </div>
+    </div>
+<!--    菜单结束-->
+<!--    为你优选开始-->
+    <div class="forU">
+
+    </div>
+<!--    为你优选结束-->
+</template>
+
+<script>
+    export default {
+        name: "BaiDu",
+    }
+</script>
+
+<style scoped>
+</style>
